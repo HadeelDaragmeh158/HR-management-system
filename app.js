19d055975762ad1a738b5c7c8c4ad9eacd9df6c6
HadeelDaragmeh158 <hadeelmohammad158@gmail.com>
2022-01-25 23:49:47 +0200

    task8add

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function Employee(EmployeeID, FullName, Department, Level, ImageURL, Salary) {
     this.Salary = Salary;
 }
 
-function getRandom(min, max) {
+ getRandom = function(min, max) {
     return Math.floor(Math.random() * (max)) + min;
 }
 function netSalary(nsal) {
@@ -33,7 +33,7 @@ for (let i = 0; i <= 2; i++) {
 }
 
 emp1 =new Employee(1000,"Ghazi Samer","Administration","Senior");
-emp2 = new Employee(1001,"Lana Ali","Finance","Senior");
+emp2 =new Employee(1001,"Lana Ali","Finance","Senior");
 emp3 =new Employee(1002,"Tamara Ayoub","Marketing","Senior");
 emp4 =new Employee(1003,"Safi Walid","Administration","Mid-Senior");
 emp5 =new Employee(1004,"Omar Zaid","Development","Senior");
@@ -47,3 +47,52 @@ emp4.render();
 emp5.render();
 emp6.render();
 emp7.render();
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+                             /* TASK8 */
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+function NewDigits()
+{
+ var emp8= emp1 =new Employee(1007,);
+ emp9.render();
+ var emp9= emp1 =new Employee(1008,);
+ emp9.render();
+ var emp10= emp1 =new Employee(1009,);
+ emp10.render();
+ var emp11= emp1 =new Employee(1010,);
+ emp11.render();
+ 
+}
+let  EmpEmp =document.getElementById('EmpEmp');
+let show=document.getElementById('Show');
+
+
+function handelSubmit(event){
+    event.preventDefault();
+    let fullName=Employee.taraget.FullName.value;
+    let Department=Employee.taraget.Department.value;
+    let Level=Employee.taraget.Level.value;
+    let Imageurl=Employee.taraget.ImageURL.value;
+   // console.log(`${FullName} ${Department} ${Level} ${Imageurl}`);
+
+   let newemp=new Employee(fullName,Department,Level,Imageurl);
+   newemp.render();
+}
+Employee.prototype.render=function()
+{
+    let div = createElement('div');
+    show.appendChild('div');
+    let img = createElement('img');
+    div.appendChild(img);
+    let h1 = createElement('h1');
+    div.appendChild(h1);
+    h1.textContent(this.FullName);
+    let p= createElement('p');
+    div.appendChild(p);
+}
+EmpEmp.addEventListener('submit', handelSubmit);
